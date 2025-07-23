bc8cc9e6b0abfdb454e70c5c5aca4908bdf0dbf0
filip patrman <filip.patrman.023@pslib.cz>
2025-07-23 12:57:26 +0200

    ghpages, readme

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  },
+  basePath: '/trampolinyharcov', // název vašeho GitHub repozitáře
+  assetPrefix: '/trampolinyharcov',
 };
 
 export default nextConfig;

3f4bc53cd0dcada40918fb846596968c1b95ff93
filip patrman <filip.patrman.023@pslib.cz>
2025-08-03 20:08:07 +0200

    info a fix nas tym

--- app/components/sections/NasTym.tsx
+++ app/components/sections/NasTym.tsx
@@ -8,12 +8,21 @@ const NasTym = () => {
   return (
     <div className="bg-secondary">
       <div className="mx-8 lg:mx-auto py-16 max-w-5xl">
+        <style jsx global>{`
+          .team-swiper .swiper-slide {
+            height: auto;
+            display: flex;
+          }
+          .team-swiper .swiper-slide > div {
+            flex: 1;
+          }
+        `}</style>
         <Swiper
           modules={[Navigation, Autoplay]}
           loop={true}
           autoplay={{
             delay: 3000,
-            disableOnInteraction: false
+            disableOnInteraction: false,
           }}
           slidesPerView={1}
           spaceBetween={16}
@@ -37,7 +46,7 @@ const NasTym = () => {
           }}
           speed={2000}
           navigation={true}
-          className="w-full"
+          className="w-full team-swiper"
         >
           <SwiperSlide>
             <div className="bg-white rounded-2xl flex flex-col items-center text-center p-8 h-full">
@@ -49,9 +58,103 @@ const NasTym = () => {
                 className="mx-auto"
               />
               <h3 className="mt-4">
+                Miroslav
+                <br />
+                Patrman
+              </h3>
+              <p className="mt-2">
+                Hlavní trenér oddílu
+                <br />
+                Vizionář a
+                <br />
+                koordinátor projektu
+              </p>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="bg-white rounded-2xl flex flex-col items-center text-center p-8 h-full">
+              <Image
+                src={"/images/suova.png"}
+                alt="tym foto"
+                width={144}
+                height={144}
+                className="mx-auto"
+              />
+              <h3 className="mt-4">
+                Klára
+                <br />
+                Patrmanová
+              </h3>
+              <p className="mt-2">
+                Manažerka
+                <br />
+                Koordinátorka
+                <br />
+                projektů a dotací
+              </p>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="bg-white rounded-2xl flex flex-col items-center text-center p-8 h-full">
+              <Image
+                src={"/images/suova.png"}
+                alt="tym foto"
+                width={144}
+                height={144}
+                className="mx-auto"
+              />
+              <h3 className="mt-4">
+                Monika
+                <br />
+                Patrmanová
+              </h3>
+              <p className="mt-2">
+                Trenérka závodních
+                <br />
+                družstev
+                <br />
+                Koordinátor
+                <br />
+                sportovních soutěží
+              </p>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="bg-white rounded-2xl flex flex-col items-center text-center p-8 h-full">
+              <Image
+                src={"/images/suova.png"}
+                alt="tym foto"
+                width={144}
+                height={144}
+                className="mx-auto"
+              />
+              <h3 className="mt-4">
+                Gabriel
+                <br />
+                Tichý
+              </h3>
+              <p className="mt-2">
+                Trenér závodních
+                <br />
+                družstev
+                <br />
+                Kondiční trenér
+              </p>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="bg-white rounded-2xl flex flex-col items-center text-center p-8 h-full">
+              <Image
+                src={"/images/suova.png"}
+                alt="tym foto"
+                width={144}
+                height={144}
+                className="mx-auto"
+              />
+              <h3 className="mt-4">
                 Michaela
                 <br />
-                Suova
+                Sůvová
               </h3>
               <p className="mt-2">
                 Komunikace
@@ -69,13 +172,41 @@ const NasTym = () => {
                 className="mx-auto"
               />
               <h3 className="mt-4">
-                Michaela
+                Eliška
                 <br />
-                Suova
+                Patrmanová
               </h3>
               <p className="mt-2">
-                Komunikace
-                <br />s partenry
+                Fyzioterapeutka
+                <br />
+                Trenérka
+                <br />
+                Transformativní
+                <br />
+                koučka
+              </p>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="bg-white rounded-2xl flex flex-col items-center text-center p-8 h-full">
+              <Image
+                src={"/images/suova.png"}
+                alt="tym foto"
+                width={144}
+                height={144}
+                className="mx-auto"
+              />
+              <h3 className="mt-4">
+                Kamila
+                <br />
+                Brücklerová
+              </h3>
+              <p className="mt-2">
+                Správce
+                <br />
+                členské základny
+                <br />
+                Trenérka
               </p>
             </div>
           </SwiperSlide>
@@ -91,52 +222,9 @@ const NasTym = () => {
               <h3 className="mt-4">
                 Michaela
                 <br />
-                Suova
+                Křiklavová
               </h3>
-              <p className="mt-2">
-                Komunikace
-                <br />s partenry
-              </p>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="bg-white rounded-2xl flex flex-col items-center text-center p-8 h-full">
-              <Image
-                src={"/images/suova.png"}
-                alt="tym foto"
-                width={144}
-                height={144}
-                className="mx-auto"
-              />
-              <h3 className="mt-4">
-                Michaela
-                <br />
-                Suova
-              </h3>
-              <p className="mt-2">
-                Komunikace
-                <br />s partenry
-              </p>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="bg-white rounded-2xl flex flex-col items-center text-center p-8 h-full">
-              <Image
-                src={"/images/suova.png"}
-                alt="tym foto"
-                width={144}
-                height={144}
-                className="mx-auto"
-              />
-              <h3 className="mt-4">
-                Michaela
-                <br />
-                Suova
-              </h3>
-              <p className="mt-2">
-                Komunikace
-                <br />s partenry
-              </p>
+              <p className="mt-2">Vedoucí recepce</p>
             </div>
           </SwiperSlide>
         </Swiper>

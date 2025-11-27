8523e010f0d046af0b924d0234d404e25ab5579c
filip patrman <filip.patrman.023@pslib.cz>
2025-11-27 07:24:16 +0100

    nadpis aktuality

--- app/components/sections/Aktuality.tsx
+++ app/components/sections/Aktuality.tsx
@@ -28,6 +28,7 @@ const Aktuality = () => {
   }
   return (
     <div className="max-w-4xl mx-auto p-8 min-w-0 w-full">
+      <h2 className="text-center mb-8">Aktuality</h2>
       <Swiper
         cssMode={true}
         navigation={true}

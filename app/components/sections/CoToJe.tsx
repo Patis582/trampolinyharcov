fe8ed0aa0c8fe45a18b5bc3fcafc3bc44cd6d3a0
filip patrman <filip.patrman.023@pslib.cz>
2026-05-26 12:41:13 +0200

    uvod text fix

--- app/components/sections/CoToJe.tsx
+++ app/components/sections/CoToJe.tsx
@@ -7,16 +7,11 @@ const CoToJe = () => {
       <div className="max-w-3xl text-center flex flex-col gap-8">
         <div>
           <h2 className="text-center mb-6">
-            Trampolínové
-            <br className="hidden sm:block" /> a pohybové centrum
+            Sportovní areál
+            <br className="hidden sm:block" />Harcov
           </h2>
           <p className="text-lg text-gray-700 leading-relaxed">
-            Cílem našeho projektu je vytvořit multifunkční sportovní centrum
-            lokalizované v přírodě, které nabídne moderní a profesionální zázemí
-            pro pohybové nadšence všech věkových kategorií a dovedností. V obou
-            halách, kde nyní trénujeme jsme v nájmu a budoucnost naší další
-            možnosti trénovat v nich je velmi nejistá.
-          </p>
+            V Liberci - Harcově tvoříme sportovní areál pro širokou veřejnost, jehož součástí budou venkovní prvky pro pohybové aktivity (volejbal, basket, workout, trampolína, slackline...) a v budoucnu také Trampolínové a pohybové centrum zaměřené na gymnastiku, parkour a trampolíny.          </p>
         </div>
         <div className="flex justify-center gap-4 flex-wrap">
           <Button href="#projekt" filled={true}>
